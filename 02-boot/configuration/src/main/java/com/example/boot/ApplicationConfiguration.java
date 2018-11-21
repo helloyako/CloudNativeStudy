cc86ac22aa70bdc1fa3a3533b310ea8da73773ff
helloyako <helloyako@gmail.com>
2018-11-22 00:14:27 +0900

    add REST API Endpoint

---
+++ 02-boot/configuration/src/main/java/com/example/boot/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
