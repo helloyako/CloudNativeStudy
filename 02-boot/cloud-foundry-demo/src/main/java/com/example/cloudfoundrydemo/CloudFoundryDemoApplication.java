1b523927f611180b61e8c70955964fa82a867ea1
helloyako <helloyako@gmail.com>
2018-11-21 23:33:43 +0900

    create cloud foundry demo project

---
+++ 02-boot/cloud-foundry-demo/src/main/java/com/example/cloudfoundrydemo/CloudFoundryDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.cloudfoundrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFoundryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFoundryDemoApplication.class, args);
+	}
+}
